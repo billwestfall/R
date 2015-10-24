b43757100dd1f2e7376f127476a4d05f2a938d99
billwestfall <billwestfall@yahoo.com>
2015-10-23 18:48:12 -0700

    Update page0483a.r
    forgot the output in the comments

--- 09chapt/page0483a.r
+++ 09chapt/page0483a.r
@@ -1,16 +1,16 @@
-# > xk <- range(x)
-# > xl <- var(x)
-# > xm <- sd(x)
+# > x <- c(1.25)
+# >
+# > xk <- pnorm(x)
+# > xl <- pnorm(x,lower.tail=FALSE)
 # >
 # > xk
-# [1]  7 63
+# [1] 0.8943502
 # > xl
-# [1] 424.4444
-# > xm
-# [1] 20.60205
+# [1] 0.1056498
 # >
 
-x <- c(1.25)
+x <- c(-1.02)
+y <- c(0.92)
 
 xk <- pnorm(x)
 xl <- pnorm(x,lower.tail=FALSE)
