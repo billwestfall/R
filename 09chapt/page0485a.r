a052a52b6f7841a0a0a5786eae10683fe07fa990
billwestfall <billwestfall@yahoo.com>
2015-10-25 12:39:53 -0700

    Create page0485a.r
    not the right answers but placeholder until work it out

---
+++ 09chapt/page0485a.r
@@ -0,0 +1,17 @@
+# > xk <- qnorm(x, mean=.5, sd=4)
+# > xl <- qnorm(y, mean=.5, sd=4)
+# >
+# > xk
+# [1] 0.09826512
+# > xl
+# [1] 1.513388
+# >
+
+x <- c(.46)
+y <- c(.6)
+
+xk <- qnorm(x, mean=.5, sd=4)
+xl <- qnorm(y, mean=.5, sd=4)
+
+xk
+xl
