d84bf54d0c923ab16e6575f11e7a1bfaadd91f27
Bill Westfall <billwestfall@yahoo.com>
2015-11-29 13:35:57 -0800

    Update celtic.r
    deal out 10 tarot cards

--- miscellaneous/tarot/celtic.r
+++ miscellaneous/tarot/celtic.r
@@ -1,20 +1,48 @@
 # MacBook-Air:r billw$ rscript celtic.r
+# [1] "Knight"
+# [1] "Coins"
+# [1] "Natural"
+# [1] "9"
+# [1] "Cups"
+# [1] "Natural"
 # [1] "Ace"
 # [1] "Wands"
 # [1] "Natural"
-# MacBook-Air:r billw$ rscript celtic.r
-# [1] "6"
-# [1] "Swords"
+# [1] "Ace"
+# [1] "Coins"
 # [1] "Natural"
-# MacBook-Air:r billw$ rscript celtic.r
-# [1] "10"
-# [1] "Cups"
+# [1] "7"
+# [1] "Swords"
 # [1] "Reversed"
+# [1] "3"
+# [1] "Wands"
+# [1] "Reversed"
+# [1] "7"
+# [1] "Coins"
+# [1] "Natural"
+# [1] "6"
+# [1] "Coins"
+# [1] "Natural"
+# [1] "Queen"
+# [1] "Swords"
+# [1] "Reversed"
+# [1] "3"
+# [1] "Coins"
+# [1] "Natural"
+# [1] "9"
+# [1] "Coins"
+# [1] "Natural"
+# MacBook-Air:r billw$
 
-pips <- c("2", "3", "4", "5", "6", "7", "8", "9", "10", "Page", "Knight", "Queen", "King", "Ace")
-suit <- c("Swords", "Cups", "Coins", "Wands")
-rev  <- c("Reversed", "Natural")
+i <- 0
+while(i < 11)
+{
+        pips <- c("2", "3", "4", "5", "6", "7", "8", "9", "10", "Page", "Knight", "Queen", "King", "Ace")
+        suit <- c("Swords", "Cups", "Coins", "Wands")
+        rev  <- c("Reversed", "Natural")
 
-sample(pips, 1)
-sample(suit, 1)
-sample(rev, 1)
+        print (sample(pips, 1))
+        print (sample(suit, 1))
+        print (sample(rev, 1))
+i <- i + 1
+}
