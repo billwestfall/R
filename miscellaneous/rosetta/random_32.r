2f3f59cd0ab59a96215aea247c89ec6546a886d6
Bill Westfall <billwestfall@yahoo.com>
2017-05-23 21:31:49 -0700

    Update random_32.r

--- miscellaneous/rosetta/random_32.r
+++ miscellaneous/rosetta/random_32.r
@@ -1,5 +1,9 @@
 # random 32 bit number with R
 # run as rscript random_32.r
+#MacBook-Air:rosetta billw$ rscript random_32.r 
+#[1] 713626785
+#MacBook-Air:rosetta billw$ rscript random_32.r 
+#[1] 1470588637
 
 x <- runif(1, 0, 2147483647)
 
