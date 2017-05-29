861ca19de1af5a077aef399722944de04fecd76d
Bill Westfall <billwestfall@yahoo.com>
2017-05-29 10:05:20 -0700

    Update windowed_app.r

--- miscellaneous/rosetta/windowed_app.r
+++ miscellaneous/rosetta/windowed_app.r
@@ -1,6 +1,5 @@
-
-library(tcltk2)
-
-win3 <- tk2toplevel(title = "Tk2 window", manage = "grid", padx = 70, pady = 30)
-
-win3$butOK <- tk2button(text = "OK", width = -6, command = TkCmd_destroy(parent))
+library(tcltk)
+win1 <- tktoplevel()
+butOK <- tkbutton(win1, text = "OK",
+  command = function() tkdestroy(win1))
+tkgrid(butOK)
